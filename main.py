ae720a052d4b4f6a681dc53239c999fba37a8a28
chris9361 <chris9361@yahoo.com>
2017-11-11 14:00:32 -0600

    update

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import random
+
+
+name=input("Hello Young Wizard.  What is your name? ")
+
+# Task 1:  Intro
+print("Hello, Wizard " + name + ".")
+# Task 2:  get players name
+
+# Task 3:  set variables
+
+# Task 4:  random number generator created
+ranNum = random.randint(1, 5)
+print(ranNum)
+
+
+# Task 5:
+
+# Task 6:
+
+# Task 7:
+
+# Task 8:
+
